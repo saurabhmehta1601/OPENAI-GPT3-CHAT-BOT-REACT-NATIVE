af590b82e30c15ef55492584f02a9e5bff60a4a9
saurabh <saurabhmehta1601@gmail.com>
2023-06-17 22:48:56 +0530

    add Message component

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "./components/Header";
+import Message from "./components/Message";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView className="bg-slate-800" style={styles.container}>
       <Header />
+      <Message text="Hello form User" sender={"user"} />
+      <Message text="Hello form Bot" sender={"bot"} />
+      <Message text="Hello form Bot" sender={"bot"} loading={true} />
     </SafeAreaView>
   );
 }
@@ -15,6 +17,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333",
   },
 });
---
+++ components/Message.js
@@ -0,0 +1,81 @@
+import { Text, View, Image, StyleSheet, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import { useInterval } from "usehooks-ts";
+import PropTypes from "prop-types";
+
+function formatTextWithPreLine(text) {
+  const lines = text.split("\n");
+
+  const formattedLines = lines.map((line, index) => {
+    const formattedLine = line.replace(/ +/g, " ");
+    return <Text key={index}>{formattedLine}</Text>;
+  });
+
+  return formattedLines;
+}
+
+const Message = (props) => {
+  const [renderedText, setRenderedText] = useState("");
+  const [cursor, setCursor] = useState(0);
+
+  useInterval(
+    () => {
+      if (cursor >= props.text.length) {
+        return;
+      }
+      setRenderedText((text) => text + props.text[cursor]);
+      setCursor((cursor) => cursor + 1);
+    },
+    cursor >= props.text.length || props.loading || props.sender !== "bot"
+      ? null
+      : 40
+  );
+
+  return (
+    <View className="flex flex-row justify-center items-center gap-x-2 px-2 py-1 rounded-md">
+      <Image
+        source={
+          props.sender === "bot"
+            ? require("../assets/bot.png")
+            : require("../assets/user.png")
+        }
+        className="w-10 h-10"
+      />
+      <View className={" rounded-sm flex-1 "}>
+        {props.loading ? (
+          <ActivityIndicator size="large" color="#fff" />
+        ) : (
+          <Text
+            className={
+              "text-lg py-2 px-3 rounded-md font-bold bg-slate-700 text-gray-200"
+            }
+          >
+            {props.sender === "bot"
+              ? renderedText
+              : formatTextWithPreLine(props.text)}
+          </Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Message;
+
+Message.propTypes = {
+  text: PropTypes.string.isRequired,
+  sender: PropTypes.oneOf(["bot", "user"]),
+  loading: PropTypes.bool,
+};
+
+Message.defaultProps = {
+  sender: "user",
+  loading: false,
+};
+
+const styles = StyleSheet.create({
+  lottie: {
+    width: 100,
+    height: 100,
+  },
+});
