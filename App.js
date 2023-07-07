63ffc29ed3b6b9e9fd07087f69ae11d0ae718ef2
saurabh <saurabhmehta1601@gmail.com>
2023-07-07 12:27:18 +0530

    remove unused imports from App.js

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 import { View, StyleSheet } from "react-native";
-import React, { useState, useCallback } from "react";
+import React, { useCallback } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "./components/Header";
-import Message from "./components/Message";
 import PromptSubmitForm from "./components/PromptSubmitForm";
-import { FlatList } from "react-native";
-import generateRandomID from "./utils/getRadomID";
-import { sender } from "./constants";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import ChatMessageSection from "./components/ChatMessageSection";
-
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,17 +17,7 @@ const customFonts = {
 
 const SERVER_URL = "https://chat-buddy-express-api-7cnof6acma-el.a.run.app";
 
-const greetingMessage = {
-  id: generateRandomID(),
-  text: "Hello, How can I help you?",
-  sender: sender.BOT,
-  loading: false,
-};
-
 export default function App() {
-  // Load fonts and return null while fonts are loaded
-
-  const [messages, setMessages] = useState([greetingMessage]);
 
   const [areFontsLoading] = useFonts(customFonts);
   const onLayoutRootView = useCallback(async () => {
@@ -53,23 +38,8 @@ export default function App() {
         style={styles.container}
         onLayout={onLayoutRootView}
       >
-
         <Header />
         <ChatMessageSection />
-        {/* <FlatList
-          data={messages}
-          renderItem={({ item }) => (
-            <View className={"my-1"}>
-              <Message
-                text={item.text}
-                sender={item.sender}
-                loading={item.loading}
-              />
-            </View>
-          )}
-          keyExtractor={(item) => item.id}
-        /> */}
-
         <View className="mt-auto">
           <PromptSubmitForm />
         </View>
