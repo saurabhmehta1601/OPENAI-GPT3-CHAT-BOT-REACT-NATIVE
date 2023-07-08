63354510e0ed21705d5ee8e8ff8d7d18700e4621
saurabh <saurabhmehta1601@gmail.com>
2023-07-08 16:05:40 +0530

    update Message styles

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,15 +1,16 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 
 interface IProps {
   IconPack: any,
   name: string
   size?: number
   color?: string
+  wrapperStyles?: StyleProp<ViewStyle>
 }
 
-export default function Avatar({ IconPack, name, size = 25, color = "#fff" }: IProps) {
+export default function Avatar({ IconPack, name, size = 25, color = "#fff", wrapperStyles = undefined }: IProps) {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, wrapperStyles]} >
       <IconPack name={name} size={size} color={color} />
     </View>
   );
